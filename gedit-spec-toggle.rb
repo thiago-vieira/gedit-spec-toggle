cd60cd0fad77ca433979ab4bf838e911cc883a32
Thiago Vieira <thiago.rails@gmail.com>
2012-04-03 09:20:09 -0300

    Refactory for use in simple app/spec project

--- gedit-spec-toggle.rb
+++ gedit-spec-toggle.rb
@@ -5,7 +5,9 @@ path = path.split("/")
 doc = path.delete(path[-1])
 
 dirs = []
+rails_struct = false
 if path.include?('app') || path.include?('spec')
+	rails_struct = true
 	path.size.times do
 		dir = path.delete(path[-1])
 		break if dir == 'app' || dir == 'spec'
@@ -15,15 +17,15 @@ end
 
 if doc.match(/erb_spec.rb/)
   doc_to_open = doc[0..-9]
-  dirs << "app" if dirs.count > 0
+  dirs << "app" if rails_struct
 elsif doc.match(/spec.rb/)
   doc_to_open = doc[0..-9] + ".rb"
-  dirs << "app" if dirs.count > 0
+  dirs << "app" if rails_struct
 elsif doc.match(/\.erb/)
   doc_to_open = doc + "_spec.rb"
-  dirs << "spec" if dirs.count > 0
+  dirs << "spec" if rails_struct
 elsif doc.match(/\.rb/)
-  dirs << "spec" if dirs.count > 0
+  dirs << "spec" if rails_struct
   doc_to_open = doc[0..-4] + "_spec.rb"
 else
   raise "The file is not one .rb, .erb or Spec of a Ruby/Rails project."
